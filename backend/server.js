49579768a271b7f55b4b563d9d8c2279dc85b64c
Sandhya <connect2sandhyaa@gmail.com>
2026-08-13 21:04:34 +0530

    Add backend service with Docker Compose

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {
+        "Content-Type": "application/json"
+    });
+
+    res.end(JSON.stringify({
+        message: "Hello from DevOps Backend!",
+        status: "success"
+    }));
+});
+
+server.listen(3000, () => {
+    console.log("Backend running on port 3000");
+});
